8530e1c6cfbf8623fc3ed7dcadb003281e2ef24a
Vincent Dubedout <vincent@dubedout.eu>
2012-05-29 23:26:04 +0200

    typo

--- jbmm-generator/src/com/andexp/jbmmgenerator/Main.java
+++ jbmm-generator/src/com/andexp/jbmmgenerator/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		generators[0] = new AsyncGenerator(1,250);
 		generators[1] = new AsyncGenerator(251,500);
 		generators[2] = new AsyncGenerator(501,750);
-		generators[3] = new AsyncGenerator(75,1000);
+		generators[3] = new AsyncGenerator(750,1000);
 		generators[4] = new AsyncGenerator(1001,1250);
 		generators[5] = new AsyncGenerator(1251,1506);
 		
